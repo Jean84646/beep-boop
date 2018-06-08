1c04335a548b2e57e718a8ca7852c511d5083e09
Jean Jia <jean84646@gmail.com>
2018-06-08 13:24:30 -0700

    numbers replaced correctly with corresponding words join by coma.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 $(document).ready(function(){
   $("#form-beep-boop").submit(function(event){
     event.preventDefault();
-    var userInput = parseInt($("input#input-number").val());
+    var userInput = $("input#input-number").val();
     var result = convert(userInput);
     $("#output-area").fadeIn();
     $("#user-output").text(result);
@@ -14,42 +14,26 @@ $(document).ready(function(){
 
 // Business Logic
 var convert = function(number){
-  var numArrays = [];
+  var result = "";
 
-  if (number === 0) {
-    return numArrays.push("Beep");
-debugger;
-  } else if (number === 1) {
-    return  numArrays.push("Boop");
-  } else if (number%3 === 0) {
-    return  "I'm sorry, Dave. I'm afraid I can't do that.";
-  } else {
-    numArrays.push(number);
-  }
-  return numArrays.toString();
+  if ((parseInt(number)) % 3 === 0) {
+    return "I'm sorry, Dave. I'm afraid I can't do that.";
+  };
 
+  var numArrays = number.split("");
+  var makeArrays = [];
+
+  for (var i = 0; i <= numArrays.length; i++) {
+    if (numArrays[i] === "0") {
+      numArrays[i] = "Beep";
+      makeArrays.push(numArrays[i]);
+    } else if (numArrays[i] === "1") {
+      numArrays[i] = "Boop";
+      makeArrays.push(numArrays[i]);
+    } else {
+      makeArrays.push(numArrays[i]);
+    };
+
+  };
+  return result = makeArrays
 };
-
-
-  // var numArrays = number.toString().split("");
-  //
-  // for (var i = 0; i <= numArrays.length; i++) {
-  //   if (numArrays[i] === "0") {
-  //     numArrays[i] = "Beep";
-  //     return makeString(numArrays[i]);
-  //   } else if (numArrays[i] === "1") {
-  //     numArrays[i] = "Boop";
-  //     return output = "Boop";
-  //   } else if (number%3 === "0") {
-  //     return output = "I'm sorry, Dave. I'm afraid I can't do that.";
-  //   } else {
-  //     return number;
-  //   };
-  //
-  // var makeString = function(numArrays){
-  // var string = "";
-  // for (i = 0; i < numArrays.length; i += 1){
-  //   string = string.concat(numArrays[i]);
-  // };
-  // return string;
-  // };
